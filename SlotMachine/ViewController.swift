2468564912c56d2ce4995f1557aedc78ec66e63a
Paul Uhn <paul.uhn@gmail.com>
2014-10-02 06:54:40 -0500

    Setup container views.

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Containers
+    var firstContainer: UIView!
+    var secondContainer: UIView!
+    var thirdContainer: UIView!
+    var fourthContainer: UIView!
+    
+    // Constants
+    let kMarginForView:CGFloat = 10.0
+    let kSixth:CGFloat = 1.0/6.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupContainerViews()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func setupContainerViews()  {
+        let x = self.view.bounds.origin.x + kMarginForView
+        let w = self.view.bounds.width - (kMarginForView * 2)
+        let h = self.view.bounds.height * kSixth
+        var y = self.view.bounds.origin.y
+        
+        self.firstContainer = UIView(frame: CGRectMake(x, y, w, h))
+        self.firstContainer.backgroundColor = UIColor.redColor()
+        self.view.addSubview(self.firstContainer)
+        
+        y += firstContainer.frame.height
+        
+        self.secondContainer = UIView(frame: CGRectMake(x, y, w, h * 3))
+        self.secondContainer.backgroundColor = UIColor.blackColor()
+        self.view.addSubview(self.secondContainer)
+        
+        y += secondContainer.frame.height
+
+        self.thirdContainer = UIView(frame: CGRectMake(x, y, w, h))
+        self.thirdContainer.backgroundColor = UIColor.lightGrayColor()
+        self.view.addSubview(self.thirdContainer)
+        
+        y += thirdContainer.frame.height
+        
+        self.fourthContainer = UIView(frame: CGRectMake(x, y, w, h))
+        self.fourthContainer.backgroundColor = UIColor.blackColor()
+        self.view.addSubview(self.fourthContainer)
+    }
 
 }
 
