84f9de978f91568ec382c1175bc768208cf98c48
pauluhn <paul.uhn@gmail.com>
2014-10-05 08:06:02 -0500

    Add SlotBrain class.

---
+++ SlotMachine/SlotBrain.swift
@@ -0,0 +1,19 @@
+//
+//  SlotBrain.swift
+//  SlotMachine
+//
+//  Created by Paul Uhn on 10/5/14.
+//  Copyright (c) 2014 Paul. All rights reserved.
+//
+
+import Foundation
+
+class SlotBrain {
+    class func computeWinnings (slots: [[Slot]]) -> Int {
+        var winnings = 0
+        var flushWinCount = 0
+        var threeOfAKindWinCount = 0
+        var straightWinCount = 0
+        for slotRow in slots {        }
+        return winnings
+    }
